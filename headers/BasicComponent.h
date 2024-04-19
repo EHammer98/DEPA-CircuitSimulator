fd5386029fb860044b5c83ea8c62fe7399915696
Elwin Hammer <elwinhamer@gmail.com>
2024-04-19 10:06:49 +0200

    Basic structure based on classdiagram 3

---
+++ headers/BasicComponent.h
@@ -0,0 +1,19 @@
+#include "LogicComponent.h"
+
+class BasicComponent : public LogicComponent {
+protected:
+    bool input1;
+    bool input2;
+    bool output;
+public:
+    virtual void computeOutput() = 0;
+};
+
+class AND : public BasicComponent {
+public:
+    void computeOutput() override {
+        output = input1 && input2;
+    }
+};
+
+// Repeat similar structure for OR, NOT, NAND, NOR, XOR
