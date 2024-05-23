d67e8a03fb75e7ae4500d1fe7718fadd03ff7777
Elwin Hammer <elwinhammer@gmail.com>
2024-05-23 13:30:58 +0200

    basic components changed

--- BasicComponents.cpp
+++ BasicComponents.cpp
@@ -1,16 +1,31 @@
-// BasicComponents.cpp
 #include "headers/BasicComponents.h"
 
+// AND gate compute function
+void AND::computeOutput() {
+    output = input1 && input2;
+}
 
-/**
- * Computes the output of the AND gate.
- *
- * @throws None
- */
-//void AND::computeOutput() {
-//    output = input1 && input2;
-//}
+// OR gate compute function
+void OR::computeOutput() {
+    output = input1 || input2;
+}
 
+// NOT gate compute function
+void NOT::computeOutput() {
+    output = !input1;  // Using input1 for NOT gate
+}
 
-//TODO
-// Add other ports as well 
+// NAND gate compute function
+void NAND::computeOutput() {
+    output = !(input1 && input2);
+}
+
+// NOR gate compute function
+void NOR::computeOutput() {
+    output = !(input1 || input2);
+}
+
+// XOR gate compute function
+void XOR::computeOutput() {
+    output = input1 ^ input2;
+}
--- headers/BasicComponents.h
+++ headers/BasicComponents.h
@@ -7,63 +7,50 @@ protected:
     bool input1;
     bool input2;
     bool output;
+
 public:
+    // Constructor for binary gates
+    BasicComponent() : input1(false), input2(false), output(false) {}
+
+    // Abstract function computeOutput from LogicComponent will be overridden in derived
     virtual void computeOutput() = 0;
-    bool getOutput() const override {
-        return output; // Implementation for getting the output
-    }
+
+    // getOutput implemented here to return the output of the basic component
+    bool getOutput() const override { return output; }
 };
 
-// AND port
+// AND gate
 class AND : public BasicComponent {
 public:
-    void computeOutput() override {
-        output = input1 && input2;
-    }
+    void computeOutput() override;
 };
 
-// OR port
+// OR gate
 class OR : public BasicComponent {
 public:
-    void computeOutput() override {
-        output = input1 || input2;
-    }
+    void computeOutput() override;
 };
 
-// NOT port
+// NOT gate, using input1 for its single input
 class NOT : public BasicComponent {
-protected:
-    bool input; // NOT gate only has one input
-
 public:
-    void computeOutput() override {
-        output = !input; // Use the single input for NOT logic
-    }
-    bool getOutput() const override {
-        return output; // Override to get the output from the single input
-    }
+    void computeOutput() override;
 };
 
-// NAND port
+// NAND gate
 class NAND : public BasicComponent {
 public:
-    void computeOutput() override {
-        output = !(input1 && input2);
-    }
+    void computeOutput() override;
 };
 
-// NOR port
+// NOR gate
 class NOR : public BasicComponent {
 public:
-    void computeOutput() override {
-        output = !(input1 || input2);
-    }
+    void computeOutput() override;
 };
 
-// XOR port
+// XOR gate
 class XOR : public BasicComponent {
 public:
-    void computeOutput() override {
-        output = input1 ^ input2;
-    }
+    void computeOutput() override;
 };
